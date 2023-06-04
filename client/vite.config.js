e45c9e0d1663bdcfab89e6a961d596c2034c7783
Charles Ukadike <charlesukadike44@gmail.com>
2023-06-04 10:02:30 +0100

    vite_config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 700,
     rollupOptions: {
       output: {
         manualChunks(id) {
