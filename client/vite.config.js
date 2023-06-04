5bca3ef84078102830660ff1e111c58eaceb510c
Charles Ukadike <charlesukadike44@gmail.com>
2023-06-04 10:55:48 +0100

    vite_config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,11 @@
 import { defineConfig } from "vite";
+import { splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   build: {
     chunkSizeWarningLimit: 700,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
   },
 });
