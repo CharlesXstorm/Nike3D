e86bf082823858652c084b13d02d88d6596b9158
Charles Ukadike <charlesukadike44@gmail.com>
2023-06-12 13:24:45 +0100

    AI configurator

--- client/src/components/AiTab.jsx
+++ client/src/components/AiTab.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import Card from "./UI/Card";
 import { motion } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
@@ -6,15 +6,44 @@ import { state } from "../store";
 import { useSnapshot } from "valtio";
 import axios from "axios";
 
-import { textAnimation } from "../utils/animationUtil";
+import { opacityAnimation, textAnimation } from "../utils/animationUtil";
 
 const Tab = () => {
   const [aiTab, setAiTab] = useState(false);
   const [prompt, setPrompt] = useState("");
   const [error, setError] = useState(null);
   const [isGenerating, setIsGenerating] = useState(false);
-
+  const [eve, setEve] = useState("Hi, I'm Eve..");
   const snap = useSnapshot(state);
+  const list = [
+    "Hi, I'm Eve.. ",
+    "your AI design assistant..",
+    "I can create designs..",
+    "just ask me..",
+    "..and I'll create it..",
+    "click me to ask.",
+  ];
+  let cnt = 0;
+
+  useEffect(() => {
+    speech();
+
+    // interval();
+
+    return;
+  }, []);
+
+  const speech = () => {
+    setTimeout(() => {
+      if (cnt >= list.length) {
+        return;
+      }
+      setEve(list[cnt]);
+      // console.log(list[cnt]);
+      cnt++;
+      speech();
+    }, [2800]);
+  };
 
   const askAI = async () => {
     try {
@@ -45,12 +74,19 @@ const Tab = () => {
   };
 
   return (
-    <div className="absolute pointer-events-none right-0 z-10  h-screen p-0 xl-p-10 flex flex-col justify-center align-center">
-      <div className="flex flex-row gap-1em justify-end pointer-events-auto">
+    <div className="absolute pointer-events-none right-0 z-10  h-screen p-0 xl-p-10 flex justify-start items-center align-center">
+      <div
+        // className={`flex ${
+        //   window.innerWidth < 1025 && !aiTab ? "flex-col" : "flex-row"
+        // } gap-1em justify-end items-end align-center pointer-events-auto w-[100%] border-2 border-solid border-red-500`}
+        className={`flex flex-row gap-1em justify-center items-center pointer-events-auto w-[100%]`}
+        style={{ transition: "all 1s linear" }}
+      >
         <AnimatePresence>
           {aiTab && (
             <motion.div
-              className="w-[180px] h-[100px] glassmorphism "
+              // className="w-[180px] h-[60px] glassmorphism "
+              className="flex flex-col justify-end items-end"
               {...textAnimation("right", "card")}
             >
               <textarea
@@ -61,26 +97,58 @@ const Tab = () => {
                   setPrompt(e.currentTarget.value);
                 }}
                 placeholder="Please enter a prompt"
-                className="bg-transparent text-white p-4 backdrop-blur-md outline-none border rounded-md w-full"
+                className="bg-transparent text-white p-4 backdrop-blur-md outline-none border rounded-md w-[180px]"
               />
               <button
                 onClick={askAI}
-                className="rounded-md border-none cursor-pointer p-4 w-full text-white bg-[dodgerblue]"
+                className="rounded-md border-none cursor-pointer p-4 w-[180px] text-white bg-[dodgerblue]"
               >
-                {isGenerating ? "Asking AI..." : error ? error : "Generate"}
+                {isGenerating ? "Asking Eve..." : error ? error : "Ask Eve"}
               </button>
             </motion.div>
           )}
         </AnimatePresence>
 
-        <button
-          className="pointer-cursor rounded-s-xl p-2 backdrop-blur-md border"
-          onClick={() => {
-            setAiTab((prev) => !prev);
-          }}
+        <div
+          className={`flex ${
+            window.innerWidth < 1025 && !aiTab ? "flex-col" : "flex-row"
+          } justify-center items-center`}
         >
-          <img width="50%" src="/ai.png" alt="ai" />
-        </button>
+          <AnimatePresence>
+            {!aiTab && (
+              <motion.div
+                {...opacityAnimation}
+                className="self-start w-[120px] p-4 backdrop-blur-md rounded-s-xl"
+              >
+                <p className="text-white text-center">{eve} </p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <div className="flex self-end flex-col">
+            <button
+              className="self-center w-[80px] pointer-cursor rounded-s-xl p-2 backdrop-blur-md border"
+              onClick={() => {
+                setAiTab((prev) => !prev);
+              }}
+              onMouseOut={() =>
+                setTimeout(() => {
+                  speech();
+                }, [1500])
+              }
+            >
+              <img width="80%" src="/ai.png" alt="ai" />
+            </button>
+            <button
+              className="self-center w-[80px] pointer-cursor rounded-s-xl p-2 backdrop-blur-md"
+              onClick={() => {
+                state.decalVisibility[snap.index] =
+                  !state.decalVisibility[snap.index];
+              }}
+            >
+              <img width="50%" src="/reload.png" alt="ai" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,104 +27,111 @@ const Header = () => {
     return () => window.removeEventListener("resize", calcWidth);
   }, []);
   return (
-    <div className="absolute pointer-events-none p-10 xl:p-20 xl:pt-5 pt-5 w-full top-0 flex justify-center items-center gap-[10%] z-10">
-      <div className="flex w-[50%] pointer-events-auto justify-start items-center">
-        <button
-          onMouseOver={() => setXdist("60px")}
-          onMouseOut={() => setXdist(isClicked ? "60px" : "0px")}
-          onClick={() => {
-            switch (
-              width[0] //checking device width
-            ) {
-              case "10%": //desktop
-                xDist === "60px" && !isClicked
-                  ? setXdist("60px")
-                  : setXdist("0px");
-                break;
-              case "30%": //tablet
-                !isClicked ? setXdist("60px") : setXdist("0px");
-                break;
-              case "60%": //mobile
-                !isClicked ? setXdist("60px") : setXdist("0px");
-                break;
-            }
-            // xDist === "60px" && !isClicked ? setXdist("60px") : setXdist("0px");
-            setIsClicked((prev) => !prev);
+    <div className="absolute pointer-events-none p-10 xl:p-20 xl:pt-5 pt-5 w-full top-0 flex justify-center items-start gap-[10%] z-10">
+      {
+        //collapsible tech stack icon
+        <div className="flex w-[50%] pointer-events-auto justify-start items-center">
+          <button
+            onMouseOver={() => setXdist("60px")}
+            onMouseOut={() => setXdist(isClicked ? "60px" : "0px")}
+            onClick={() => {
+              switch (
+                width[0] //checking device width
+              ) {
+                case "10%": //desktop
+                  xDist === "60px" && !isClicked
+                    ? setXdist("60px")
+                    : setXdist("0px");
+                  break;
+                case "30%": //tablet
+                  !isClicked ? setXdist("60px") : setXdist("0px");
+                  break;
+                case "60%": //mobile
+                  !isClicked ? setXdist("60px") : setXdist("0px");
+                  break;
+              }
+              // xDist === "60px" && !isClicked ? setXdist("60px") : setXdist("0px");
+              setIsClicked((prev) => !prev);
 
-            state.isTech = !snap.isTech;
-          }}
-          style={{ width: `${width[0]}` }}
-        >
-          <svg
-            version="1.1"
-            id="logo"
-            x="0px"
-            y="0px"
-            width="100%"
-            //   height="200px"
-            viewBox="0 0 200 200"
-            enableBackground="new 0 0 200 200"
-            fill="#fff"
-            // stroke={"#fff"}
+              state.isTech = !snap.isTech;
+            }}
+            style={{ width: `${width[1]}` }}
           >
-            <text
-              transform="matrix(1 0 0 1 39.25 162)"
-              fontFamily="Montserrat"
-              fontSize="24"
+            <svg
+              version="1.1"
+              id="logo"
+              x="0px"
+              y="0px"
+              width="100%"
+              //   height="200px"
+              viewBox="0 0 200 200"
+              enableBackground="new 0 0 200 200"
+              fill="#fff"
+              // stroke={"#fff"}
             >
-              XSTORM
-            </text>
-            <text
-              transform="matrix(1 0 0 1 39.25 77)"
-              fontFamily="Montserrat"
-              fontSize="48"
-            >
-              DEV
-            </text>
-            <path
-              // initial={{ x: 0, pathLength: 1 }}
-              // whileHover={{ x: 100, pathLength: 1 }}
-              // transition={{ duration: 0.5 }}
-              style={{
-                transform: `translateX(${xDist})`,
-                transition: "all 0.1s linear",
-              }}
-              fillRule="evenodd"
-              stroke={"#fff"}
-              clipRule="evenodd"
-              d="M125.25,103.6c0,0.221-4.478,0.4-10,0.4h-80c-5.523,0-10-0.18-10-0.4V100.4
+              <text
+                transform="matrix(1 0 0 1 39.25 162)"
+                fontFamily="Montserrat"
+                fontSize="24"
+              >
+                XSTORM
+              </text>
+              <text
+                transform="matrix(1 0 0 1 39.25 77)"
+                fontFamily="Montserrat"
+                fontSize="48"
+              >
+                DEV
+              </text>
+              <path
+                // initial={{ x: 0, pathLength: 1 }}
+                // whileHover={{ x: 100, pathLength: 1 }}
+                // transition={{ duration: 0.5 }}
+                style={{
+                  transform: `translateX(${xDist})`,
+                  transition: "all 0.1s linear",
+                }}
+                fillRule="evenodd"
+                stroke={"#fff"}
+                clipRule="evenodd"
+                d="M125.25,103.6c0,0.221-4.478,0.4-10,0.4h-80c-5.523,0-10-0.18-10-0.4V100.4
 	c0-0.221,4.477-0.4,10-0.4h80c5.522,0,10,0.18,10,0.4V103.6z"
-            />
-            <path
-              style={{
-                transform: `translateX(${"-" + xDist})`,
-                transition: "all 0.1s linear",
-              }}
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M174.75,121.1c0,0.221-6.694,0.4-14.95,0.4H40.2c-8.257,0-14.95-0.18-14.95-0.4
+              />
+              <path
+                style={{
+                  transform: `translateX(${"-" + xDist})`,
+                  transition: "all 0.1s linear",
+                }}
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M174.75,121.1c0,0.221-6.694,0.4-14.95,0.4H40.2c-8.257,0-14.95-0.18-14.95-0.4
 	V117.9c0-0.221,6.693-0.4,14.95-0.4h119.6c8.256,0,14.95,0.18,14.95,0.4V121.1z"
-            />
-          </svg>
-        </button>
-      </div>
-      <div className="flex w-[50%] justify-end items-center ">
-        <svg
-          version="1.1"
-          id="cart"
-          x="0px"
-          y="0px"
-          width={width[1]}
-          //   height="200px"
-          viewBox="0 0 200 200"
-          enableBackground="new 0 0 200 200"
-          fill="#fff"
-        >
-          <path
-            id="basket"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M62.026,122.281c-5.575-26.326-11.175-52.536-16.614-78.778
+              />
+            </svg>
+          </button>
+        </div>
+      }
+      {
+        //cart and fvourite icon
+        <div className="flex flex-col w-[50%] gap-1  justify-end items-end ">
+          <button style={{ width: `${width[1]}` }}>
+            <svg
+              version="1.1"
+              id="cart"
+              x="0px"
+              y="0px"
+              // width={width[1]}
+              width={"100%"}
+              //   height="200px"
+              viewBox="0 0 200 200"
+              enableBackground="new 0 0 200 200"
+              fill="#fff"
+            >
+              <path
+                id="basket"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M62.026,122.281c-5.575-26.326-11.175-52.536-16.614-78.778
 	c-0.617-2.977-1.992-3.708-4.758-3.59c-4.324,0.184-8.672,0.192-12.987-0.077c-1.138-0.071-2.195-1.436-3.29-2.206
 	c1.025-0.843,2.025-2.373,3.081-2.413c5.989-0.229,12.001-0.288,17.98,0.051c1.302,0.073,2.516,1.982,3.725,3.097
 	c0.204,0.188,0.169,0.63,0.252,0.952c2.853,11.14,2.853,11.144,14.59,11.139c35.322-0.013,70.645-0.03,105.967-0.043
@@ -138,33 +145,62 @@ const Header = () => {
 	c4.315,20.519,8.736,41.017,12.909,61.564c0.704,3.465,2.649,3.524,5.325,3.52c27.304-0.054,54.608-0.106,81.911,0.031
 	c3.479,0.018,4.916-0.9,5.617-4.442c3.039-15.343,6.418-30.618,9.646-45.924c1.184-5.615,2.294-11.246,3.478-17.066
 	C132.169,55.626,92.823,55.626,53.354,55.626z"
-          />
-          <circle
-            id="tire2"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            fill="none"
-            stroke="#fff"
-            strokeWidth="4"
-            strokeMiterlimit="10"
-            cx="77.286"
-            cy="158"
-            r="9.5"
-          />
-          <circle
-            id="tire1"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            fill="none"
-            stroke="#fff"
-            strokeWidth="4"
-            strokeMiterlimit="10"
-            cx="139.173"
-            cy="158"
-            r="9.5"
-          />
-        </svg>
-      </div>
+              />
+              <circle
+                id="tire2"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                fill="none"
+                stroke="#fff"
+                strokeWidth="4"
+                strokeMiterlimit="10"
+                cx="77.286"
+                cy="158"
+                r="9.5"
+              />
+              <circle
+                id="tire1"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                fill="none"
+                stroke="#fff"
+                strokeWidth="4"
+                strokeMiterlimit="10"
+                cx="139.173"
+                cy="158"
+                r="9.5"
+              />
+            </svg>
+          </button>
+          <button
+            className="flex flex-row justify-center items-center"
+            style={{ width: width[1] }}
+          >
+            <svg
+              version="1.1"
+              id="heartsvg"
+              x="0px"
+              y="0px"
+              width="80%"
+              viewBox="0 0 200 200"
+              enableBackground="new 0 0 200 200"
+            >
+              <path
+                fillRule="evenodd"
+                fill="#fff"
+                clipRule="evenodd"
+                d="M100.187,44.869c1.772-1.672,3.641-3.521,5.601-5.267
+	c6.774-6.033,14.203-11.009,22.797-14.132c8.406-3.055,16.982-3.169,25.654-1.625c10.779,1.919,20.257,6.461,28.029,14.285
+	c7.71,7.76,12.232,17.175,14.041,27.887c3.078,18.235-1.45,34.704-11.847,49.753c-5.935,8.592-12.997,16.204-20.838,23.031
+	c-10.329,8.994-20.919,17.688-31.399,26.509c-8.909,7.498-17.537,15.282-25.143,24.146c-3.581,4.174-10.287,4.392-13.799,0.309
+	c-9.19-10.687-19.832-19.798-30.609-28.788c-9.181-7.658-18.401-15.279-27.333-23.221c-8.782-7.808-16.611-16.513-22.71-26.655
+	C5.539,99.309,2.091,86.586,2.977,72.812c0.829-12.909,5.114-24.451,14.146-33.953c7.876-8.287,17.666-12.919,28.808-15.056
+	c15.897-3.049,29.847,1.318,42.389,10.973C92.387,37.906,96.158,41.424,100.187,44.869z"
+              />
+            </svg>
+          </button>
+        </div>
+      }
     </div>
   );
 };
